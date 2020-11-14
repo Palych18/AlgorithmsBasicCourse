932fe5cc355cda197787094fce98e056d4ab67d2
Palych18 <pavel_egoroff@mail.ru>
2020-11-14 23:02:10 +0300

    Задание ко второму уроку.

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        //Павел Егоров
+        //3. **Исполнитель «Калькулятор» преобразует целое число, записанное на экране. 
+        //У исполнителя две команды, каждой присвоен номер:
+        //1. Прибавь 1.
+        //2. Умножь на 2.
+
+        static void Main(string[] args)
+        {
+            int Progr1(int s, int e, int p, int m)
+            {
+                if (e < s) return 0;
+                if (e == s) return 1;
+                return Progr1(s + p, e, p, m) + Progr1(s * m, e, p, m);
+            }
+
+            Console.WriteLine(Progr1(3, 20, 1, 2));
+            Console.ReadKey();
+        }
+    }
+}
