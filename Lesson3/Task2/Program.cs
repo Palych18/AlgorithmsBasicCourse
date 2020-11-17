863e304b6b198c340923708427b2bda1e12c8ee2
Palych18 <pavel_egoroff@mail.ru>
2020-11-17 20:36:14 +0300

    Домашнее задание к третьему уроку.

---
+++ Lesson3/Task2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        //Павел Егоров
+        //2. *Реализовать шейкерную сортировку.
+
+        static void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+
+        static int[] ShakerSort(int[] array)
+        {
+            for (var i = 0; i < array.Length / 2; i++)
+            {
+                var swapFlag = false;
+                
+                for (var j = i; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        Swap(ref array[j], ref array[j + 1]);
+                        swapFlag = true;
+                    }
+                }
+
+                for (var j = array.Length - 2 - i; j > i; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        Swap(ref array[j - 1], ref array[j]);
+                        swapFlag = true;
+                    }
+                }
+
+                if (!swapFlag)
+                {
+                    break;
+                }
+            }
+
+            return array;
+        }
+
+        static void Main(string[] args)
+        {
+            var parts = new[] { 1, 2, 4, 3, 8, 5, 7, 6, 9, 0 };
+            var array = new int[parts.Length];
+            for (int i = 0; i < parts.Length; i++)
+            {
+                array[i] = Convert.ToInt32(parts[i]);
+            }
+
+            Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", ShakerSort(array)));
+
+            Console.ReadLine();
+        }
+    }
+}
