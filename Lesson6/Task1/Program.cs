a4f39486e0b3666a7eac844d9b4bd2020d4d2ca5
Palych18 <pavel_egoroff@mail.ru>
2020-11-28 22:53:26 +0300

    Домашнее задание к шестому уроку.

---
+++ Lesson6/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        //Павел Егоров
+        //1. Реализовать простейшую хеш-функцию. На вход функции подается строка, на выходе сумма кодов символов.
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            String inputString = Console.ReadLine();
+            int summ = 0;
+            foreach (var item in inputString)
+            {
+                summ += (int)item;
+            }
+            Console.WriteLine($"Хеш строки [{inputString}] равен {summ}");
+            Console.ReadKey();
+        }
+    }
+}
